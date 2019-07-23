1b7b01fb0205a02700953a6951949a8a666be5e8
Mark Lipkin <lipkin_mark@mail.ru>
2019-07-23 20:00:20 +0300

    init

---
+++ fizzbutz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace fizzbutz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 21; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FIzzBuzz");
+                }
+                
+                else if (i % 5 == 0)
+
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
